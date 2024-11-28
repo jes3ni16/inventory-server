d5858add070b6d0967029cbae3ee07a039241445
Jestoni <afacebu.jestoni@gmail.com>
2024-11-28 23:40:16 +0800

    as

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ app.get('/', (req, res) => {
 
 
 
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+  // app.listen(port, () => {
+  //   console.log(`Example app listening on port ${port}`)
+  // })
 
 
   mongoose.connect(mongoURI)
