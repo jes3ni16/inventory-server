e093d40b90330d6fe1bf406cb5b191a9c67964fa
Jestoni <afacebu.jestoni@gmail.com>
2024-11-29 19:13:27 +0800

    as1

--- index.js
+++ index.js
@@ -7,46 +7,6 @@ const tableRoutes = require('./routes/table.route');
 const transactionRoutes = require('./routes/transaction.route');
 require('dotenv').config();
 
-// Create a Redis client
-const { createClient } = require('@redis/client');
-const client = createClient();
-
-// Connect to Redis
-async function connectRedis() {
-  try {
-    // Connect to Redis server
-    await client.connect();
-    console.log('Connected to Redis!');
-    
-    // Set a key-value pair in Redis with an expiration time (3600 seconds = 1 hour)
-    await client.set('my_key', 'Hello, Redis!', { EX: 3600 });
-    console.log('Data set in Redis');
-    
-    // Get the value of 'my_key' from Redis
-    const value = await client.get('my_key');
-    console.log('Value from Redis:', value); // Should print "Hello, Redis!"
-  } catch (err) {
-    console.error('Error connecting to Redis:', err);
-  }
-}
-
-// Disconnect from Redis gracefully
-async function disconnectRedis() {
-  try {
-    await client.quit();
-    console.log('Disconnected from Redis!');
-  } catch (err) {
-    console.error('Error disconnecting from Redis:', err);
-  }
-}
-
-// Call the functions to connect, perform operations, and disconnect
-async function run() {
-  await connectRedis();
-  await disconnectRedis();
-}
-
-run().catch(err => console.error('Error in Redis operations:', err));
 
 // MongoDB connection string
 const mongoURI = process.env.MONGO_URI;
