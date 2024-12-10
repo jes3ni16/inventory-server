1a1bf3448faa482d9b772b97b6a7eab8adf26e69
Jestoni Brion <afacebu.jestoni@gmail.com>
2024-12-10 20:58:25 +0800

    Initial 2221s

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const mongoURI = process.env.MONGO_URI;
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: '*', // Allow all origins
+  origin: 'http://localhost:5173', // Allow all origins
   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
